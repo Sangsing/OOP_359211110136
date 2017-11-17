6ecdb266f3330a6095cf5508677ad3960a245764
sangsing <miulove_25_38_@hotmail.com>
2017-11-17 11:23:02 +0700

    Successfuly information data

--- src/myFirstJAVA.java
+++ src/myFirstJAVA.java
@@ -1,5 +1,10 @@
+import javax.xml.stream.FactoryConfigurationError;
+
 public class myFirstJAVA {
     public static void main(String[] args) {
         System.out.println("Hello Word, I'm Pornpimol");
+        System.out.println("Information System");
+        System.out.println("Faculty of Management Technololgy");
+        System.out.println("RMUTSV");
     }
 }
