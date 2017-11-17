3dd2dc20e1d79d9f6ad3e13cc714854029c61588
sangsing <miulove_25_38_@hotmail.com>
2017-11-17 11:29:04 +0700

    Successfuly information data

--- src/myFirstJAVA.java
+++ src/myFirstJAVA.java
@@ -4,7 +4,6 @@ public class myFirstJAVA {
     public static void main(String[] args) {
         System.out.println("Hello Word, I'm Pornpimol");
         System.out.println("Information System");
-        System.out.println("Faculty of Management Technololgy");
-        System.out.println("RMUTSV");
+
     }
 }
